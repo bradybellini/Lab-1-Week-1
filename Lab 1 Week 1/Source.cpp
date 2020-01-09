da6b8029f77caaf13e2f9e46349529d4607c3941
bradybellini <bellinibrady65@gmail.com>
2020-01-08 17:39:31 -0700

    small

--- Lab 1 Week 1/Source.cpp
+++ Lab 1 Week 1/Source.cpp
@@ -2,31 +2,32 @@
 //I certify this is my own work
 
 #include <iostream>
-#include <random>
 #include <time.h>
 
 using namespace std;
 
 int main() {
-
 	int number_list[100];
-	int r, steps;
-	int largest = 0;
+	int r;
+	int steps = 0;
+	int largest_num = 0;
 	srand(time(NULL));
 
 	for (int i = 0; i < 100; i++) {
 		r = rand() % 100;
 		number_list[i] = r;
+		cout << number_list[i] << endl;
 	}
 
 	for (int i = 0; i < size(number_list); i++) {
-		if (number_list[i] > largest) {
-			largest = number_list[i];
+		if (number_list[i] > largest_num) {
+			largest_num = number_list[i];
 		}
 		steps++;
 	}
-
-	cout << largest << endl;
+	cout << endl;
+	cout << "Step counter: " << steps << endl;
+	cout << "Largest number" << largest_num << endl;
 
 	return 0;
 }
