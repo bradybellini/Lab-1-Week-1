836bfc00a9f02734b348a6b2e73fa29368486ff3
Brady <bellinibrady65@gmail.com>
2020-01-08 17:19:23 -0700

    small

---
+++ Lab 1 Week 1/Source.cpp
@@ -0,0 +1,26 @@
+//Brady Bellini
+//I certify this is my own work
+
+#include <iostream>
+#include <random>
+#include <time.h>
+
+using namespace std;
+
+int main() {
+
+	int number_list[100];
+	int r;
+	srand(time(NULL));
+
+	for (int i = 0; i < 100; i++) {
+		r = rand() % 100;
+		number_list[i] = r;
+	}
+
+	for (int i = 0; i < )
+
+
+
+	return 0;
+}
